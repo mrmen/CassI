bdaa719bc288709aa1ce7fe9a65a949a6f63b174
Thomas Etcheverria <tetcheve@gmail.com>
2018-12-09 23:38:55 +0100

    Initial Commit

---
+++ CassITests/CassITests.swift
@@ -0,0 +1,34 @@
+//
+//  CassITests.swift
+//  CassITests
+//
+//  Created by Thomas Etcheverria on 09/12/2018.
+//  Copyright © 2018 Belette Team. All rights reserved.
+//
+
+import XCTest
+@testable import CassI
+
+class CassITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
